b229c5d219e87fffaa320c8f2f2621dc28589409
AlenaBorodacheva <alien_borodach@mail.ru>
2022-04-23 17:39:02 +0300

    Добавьте файлы проекта.

---
+++ ManageStaffDbApp/ViewModel/DataManageVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageStaffDbApp.ViewModel
+{
+    public class DataManageVM
+    {
+        
+    }
+}
